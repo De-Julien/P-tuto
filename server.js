9e3e6a3845e8e0342b8656e1e0a5817aa9bb2430
Julien <denneulin.julien@gmail.com>
2023-07-05 04:17:23 +0200

    serveur lancé réglage et commentaire

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
+// importations des modules.
 const express = require('express');
+require('dotenv').config();
+
+// Utilise express pour chaque appel de app.
 const app = express();
+
+// Le serveur écoute les requêtes sur le port désigné.
+app.listen(process.env.PORT, () => {
+    console.log(`Actuellement sur le port ${process.env.PORT}.`);
+});
